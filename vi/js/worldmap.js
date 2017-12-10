12a98e6dc7aa3488ef005ad1d98e168b0a8edd9a
Daniel Carmo <DanyBoss@users.noreply.github.com>
2017-12-10 20:47:48 +0000

    worldmap fix

--- vi/js/worldmap.js
+++ vi/js/worldmap.js
@@ -118,7 +118,7 @@ function genWorldMap(){
         g.attr("transform", "translate(" + t + ")scale(" + s + ")");
 
         //adjust the country hover stroke width based on zoom level
-        d3.selectAll(".country").style("stroke-width", 1.5 / s);
+        d3.selectAll(".country").style("stroke-width", 1 / s);
     }
     
     var throttleTimer;
