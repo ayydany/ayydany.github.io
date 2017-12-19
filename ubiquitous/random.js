3e9ba9f3f264a7a7a319f516183af9762307c6d6
Daniel Carmo <DanyBoss@users.noreply.github.com>
2017-12-19 15:27:24 +0000

    Updated ubiquitous startpage.

--- ubiquitous/random.js
+++ ubiquitous/random.js
@@ -1,14 +1,17 @@
-var maxNumber = 4;
+
+//default max values for the randomizer function,
+//if you have more then 4 images in the folder change these numbers
+var maxNumber = 3;
 var minNumber = 1;
 
 randomizeBackground();
 
-function randomNumber(){
-    return Math.floor((Math.random() * maxNumber) + minNumber);
+function randomNumber(max = maxNumber, min = minNumber){
+    return Math.floor((Math.random() * max) + min);
 }
 
 function randomizeBackground(){
-    $("html").css("background", "url(res/backgrounds/bg" + randomNumber() + ".jpg) no-repeat center center fixed");
+    $("html").css("background", "url(res/bgs/bg" + randomNumber() + ".jpg) no-repeat center center fixed");
 }
 
 function randomizeImage(){
