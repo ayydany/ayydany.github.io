2a77aeff20411163197188403b5bbd3060e46753
Daniel Carmo <DanyBoss@users.noreply.github.com>
2018-04-20 12:02:35 +0100

    updated some worldmap vars and relationship

--- olympics/js/worldmap.js
+++ olympics/js/worldmap.js
@@ -1,14 +1,15 @@
 var MAX_SELECTED_COUNTRIES = 4;
 var currentSelectedCountriesNumber = 1;
 var isZoomed = false;
+
+var ALERT_MESSAGE = "You can't select more countries because the limit was reached!\nTo start a new group from scratch try Control + Left Click"
 var NOT_SELECTED_COUNTRY_COLOR = "#A8A39D"
 
 function genWorldMap() {
     // Define size of map group
     // Full world map is 2:1 ratio
     // Using 12:5 because we will crop top and bottom of map
-    var margin = {top: 50, right: 50, bottom: 50, left: 50},
-        width = 2400,
+    let width = 2400,
         height = 1000,
         minZoom,
         maxZoom,
@@ -19,24 +20,24 @@ function genWorldMap() {
         currentZoom;
 
     // Define map projection
-    var projection = d3.geoMercator()
+    let projection = d3.geoMercator()
         .center([0, 20]) // set centre to further North as we are cropping more off bottom of map
         .scale([width / (2 * Math.PI)]) // scale to fit group width
         .translate([width / 2, height / 2]); // ensure centred in group;
 
     // Define map path
-    var path = d3.geoPath()
+    let path = d3.geoPath()
         .projection(projection);
 
     // Define map zoom behaviour
-    var zoom = d3.zoom()
+    let zoom = d3.zoom()
         .on("zoom", zoomed);
 
     //offsets for tooltips
-    var c = document.getElementById('worldmap');
-    var offsetL = c.offsetLeft+20;
-    var offsetT = c.offsetTop+10;
-    var tooltip = d3.select("#worldmap").append("div").attr("class", "tooltip hidden");
+    let c = document.getElementById('worldmap'),
+        offsetL = c.offsetLeft+20,
+        offsetT = c.offsetTop+10,
+        tooltip = d3.select("#worldmap").append("div").attr("class", "tooltip hidden");
 
     function initiateZoom() {
         // Define a "minzoom" whereby the "Countries" is as small possible without leaving white space at top/bottom or sides  
@@ -59,9 +60,9 @@ function genWorldMap() {
     // on window resize
     $(window).resize(function () {
         // Resize SVG
-        svg
-            .attr("width", $("#worldmap").width())
-            .attr("height", $("#worldmap").height());
+        sv.attr("width", $("#worldmap").width())
+          .attr("height", $("#worldmap").height());
+
         initiateZoom();
     });
 
@@ -82,15 +83,16 @@ function genWorldMap() {
 
 
     svg.append('defs')
-    .append('pattern')
-      .attr('id', 'diagonalHatch')
-      .attr('patternUnits', 'userSpaceOnUse')
-      .attr('width', 4)
-      .attr('height', 4)
-      .append('path')
-      .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
-      .attr('stroke', '#000000')
-      .attr('stroke-width', 1);
+        .append('pattern')
+        .attr('id', 'diagonalHatch')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 4)
+        .attr('height', 4)
+        .append('path')
+        .attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
+        .attr('stroke', '#000000')
+        .attr('stroke-width', 1);
+    
     // get map data
     d3.json("js/worldmap/simple_map.json", function (json) {
         //Bind data and create one path per GeoJSON feature
@@ -107,28 +109,34 @@ function genWorldMap() {
             .attr("class", function (d) {
                 if(getCountryID(d.properties.name_long) == -1)
                     return "non-selectable-country";
-                else
-                    if(d.properties.name_long == "France"){ //ugly hack for the initial state
+                else {
+                    if(d.properties.name_long == "France") { //ugly hack for the initial state
                         return "country country-on";
-                    } else 
+                    } else {
                         return "country";
+                    }
+                }
             })
             .attr("fill", function(d) {
-                if(d.properties.name_long == "France"){ //ugly hack for the initial state
-                    return color(convertNameToIOCCode(d.properties.name_long))
+                if(d.properties.name_long == "France") { //ugly hack for the initial state
+                    return color(convertNameToIOCCode(d.properties.name_long));
                 }
-                if (d3.select(this).classed("country"))
+                if (d3.select(this).classed("country")){
                     return NOT_SELECTED_COUNTRY_COLOR;
-                else
+                }
+                else {
                     return "url(#diagonalHatch)";
+                }
             })
             // add a mouseover action to show name label for feature/country
             .on("mouseover", function (d) {
-                d3.select(this).attr("stroke", function() { return getCSSColor('--main-white-color') })
+                d3.select(this).attr("stroke", function() { return getCSSColor('--main-white-color') });
 
                 this.parentElement.appendChild(this);
 
-                var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+                var mouse = d3.mouse(svg.node()).map( function(d) { 
+                    return parseInt(d); 
+                });
                 
                 tooltip.classed("hidden", false)
                     .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
@@ -139,7 +147,7 @@ function genWorldMap() {
                 tooltip.classed("hidden", true);
             })
             .on("click", function (d) {
-                if (d3.select(this).classed("country")){
+                if (d3.select(this).classed("country")) {
                     if(d3.event.ctrlKey) {
                         if (isZoomed && d3.select(this).classed("country-on")) {
                             zoomOut();
@@ -147,11 +155,11 @@ function genWorldMap() {
                         }
                         else {
                             d3.selectAll(".country").classed("country-on", false);
-                            d3.selectAll(".country").attr("fill", function(d){
+                            d3.selectAll(".country").attr("fill", function(d) {
                                 return NOT_SELECTED_COUNTRY_COLOR;
                             })
                             d3.select(this).classed("country-on", true);
-                            d3.select(this).attr("fill", function(d){
+                            d3.select(this).attr("fill", function(d) {
                                 return color(convertNameToIOCCode(d.properties.name_long));
                             })
                             currentSelectedCountriesNumber = 1;
@@ -160,10 +168,11 @@ function genWorldMap() {
                             isZoomed = true;
                         }
                     }
-                    if (!(getNumberOfCountriesInSelection() == 1 && countrySelection.includes(convertNameToIOCCode(d.properties.name_long)))) {
+                    if (!(getNumberOfCountriesInSelection() == 1 
+                      && countrySelection.includes(convertNameToIOCCode(d.properties.name_long)))) {
                         if (d3.select(this).classed("country-on")) {
                             d3.select(this).classed("country-on", false);
-                            d3.select(this).attr("fill", function(d){
+                            d3.select(this).attr("fill", function(d) {
                                 return NOT_SELECTED_COUNTRY_COLOR;
                             })
                             currentSelectedCountriesNumber--;
@@ -178,7 +187,7 @@ function genWorldMap() {
                                 currentSelectedCountriesNumber++;
                                 addCountryToSelection(d.properties.name_long);
                             } else {
-                                alert("Maximum number of countries selected is 4\nTo start a new group try Control + Left Click");
+                                alert(ALERT_MESSAGE);
                             }
                         }
                     }
@@ -186,6 +195,7 @@ function genWorldMap() {
             });
         initiateZoom();
     });
+
     // apply zoom to countriesGroup
     function zoomed() {
         t = d3.event.transform;
@@ -196,14 +206,14 @@ function genWorldMap() {
 
     function zoomOut() {
         svg.transition()
-        .duration(500)
-        .call(zoom.transform, d3.zoomIdentity.translate(midX, midY).scale(minZoom));
+            .duration(500)
+            .call(zoom.transform, d3.zoomIdentity.translate(midX, midY).scale(minZoom));
     };
 
     // zoom to show a bounding box, with optional additional padding as percentage of box size
     function boxZoom(box, centroid, paddingPerc) {
-        var svg_width = $("#worldmap").width() - 10;
-        var svg_height = $("#worldmap").height() - 10
+        let svg_width = $("#worldmap").width() - 10;
+        let svg_height = $("#worldmap").height() - 10;
         
         minXY = box[0];
         maxXY = box[1];
@@ -211,11 +221,13 @@ function genWorldMap() {
         // find size of map area defined
         zoomWidth = Math.abs(minXY[0] - maxXY[0]);
         zoomHeight = Math.abs(minXY[1] - maxXY[1]);
-       // find midpoint of map area defined
+        
+        // find midpoint of map area defined
         zoomMidX = centroid[0];
         zoomMidY = centroid[1];
         currentX = centroid[0];
         currentY = centroid[1];
+        
         // increase map area to include padding
         zoomWidth = zoomWidth * (1 + paddingPerc / 100);
         zoomHeight = zoomHeight * (1 + paddingPerc / 100);
@@ -224,11 +236,14 @@ function genWorldMap() {
         maxXscale = svg_width / zoomWidth;
         maxYscale = svg_height / zoomHeight;
         zoomScale = Math.min(maxXscale, maxYscale);
+        
         // handle some edge cases
         // limit to max zoom (handles tiny countries)
         zoomScale = Math.min(zoomScale, maxZoom);
+        
         // limit to min zoom (handles large countries and countries that span the date line)
         zoomScale = Math.max(zoomScale, minZoom);
+        
         // Find screen pixel equivalent once scaled
         offsetX = zoomScale * zoomMidX;
         offsetY = zoomScale * zoomMidY;
@@ -236,11 +251,12 @@ function genWorldMap() {
         // Find offset to centre, making sure no gap at left or top of holder
         dleft = Math.min(0, svg_width / 2 - offsetX);
         dtop = Math.min(0, svg_height/ 2 - offsetY);
+        
         // Make sure no gap at bottom or right of holder
         dleft = Math.max(svg_width - width * zoomScale, dleft);
         dtop = Math.max(svg_height - height * zoomScale, dtop);
+        
         // set zoom
-
         currentZoom = zoomScale;
         svg.transition()
             .duration(500)
