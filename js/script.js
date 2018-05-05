b719c0090c5d7764f89c5074b159409d9fa9610f
Daniel Carmo <DanyBoss@users.noreply.github.com>
2018-05-05 23:11:39 +0100

    some more cosmetic changes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,23 @@
- // header scroll
- $(window).on('scroll',function(){
+
+$(window).on('scroll',function(){
  
     if ($(window).scrollTop() >= 50) {
         $('.head').css({
          'box-shadow' : '3px 4px 5px rgba(0,0,0,0.3)',
          'height' : '0px',
          'transition' : 'ease-in-out .2s'
-        })    
-        $('.combin').css({
-            'font-size' : '25px',
-            'margin' : '5px',
         })
         $('.centertext').css({
+            'transition' : 'ease-in-out .2s',
             'display' : 'none',
-        })   
-        $('.arrow').css({
-            'display' : 'none',
-        });  
+        })
     } else {
         $('.head').css({
             'height' : '100%',
             'box-shadow' : '3px 4px 5px rgba(0,0,0,0.0)'
         })
-        $('.combin').css({
-            'font-size' : '30px',
-            'margin' : '25px',
-        })
         $('.centertext').css({
             'display' : 'block',
         })   
-        $('.arrow').css({
-            'display' : 'block',
-        });
     }
 });
