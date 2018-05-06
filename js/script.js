507075f31b0d8997187fbbc8e9a2ae9e0c4662d0
Daniel Carmo <DanyBoss@users.noreply.github.com>
2018-05-06 11:25:00 +0100

    Updated overall design and added responsive support.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
+$(document).ready(function(){
 
-$(window).on('scroll',function(){
- 
-    if ($(window).scrollTop() >= 50) {
-        $('.head').css({
-         'box-shadow' : '3px 4px 5px rgba(0,0,0,0.3)',
-         'height' : '0px',
-         'transition' : 'ease-in-out .2s'
-        })
-        $('.centertext').css({
-            'transition' : 'ease-in-out .2s',
-            'display' : 'none',
-        })
-    } else {
-        $('.head').css({
-            'height' : '100%',
-            'box-shadow' : '3px 4px 5px rgba(0,0,0,0.0)'
-        })
-        $('.centertext').css({
-            'display' : 'block',
-        })   
-    }
+    // Instantiate new modal
+    $(".element").on("click", function(){
+        var modal = new Custombox.modal({
+            content: {
+                effect: "blur",
+                target: String($(this).data().name)
+            }
+          }).open();
+    });
+    
+  
+    $(window).on('scroll', function(){
+   
+      if ($(window).scrollTop() >= 50) {
+          $('.head').css({
+           'box-shadow' : '3px 4px 5px rgba(0,0,0,0.3)',
+           'height' : '0px',
+           'transition' : 'ease-in-out .2s'
+          })
+          $('.centertext').css({
+              'transition' : 'ease-in-out .2s',
+              'display' : 'none',
+          })
+      } else {
+          $('.head').css({
+              'height' : '100%',
+              'box-shadow' : '3px 4px 5px rgba(0,0,0,0.0)'
+          })
+          $('.centertext').css({
+              'display' : 'block',
+          })   
+      }
+  });
+
 });
